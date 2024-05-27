65b23457d2efcc87af1dcb4b7de415ba1cd4d230
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-27 17:28:21 +0530

    Added: `src/commands/warn.py`

---
+++ src/commands/warn.py
@@ -0,0 +1,23 @@
+# File: warn.py
+
+import discord
+
+async def warn_user(user, reason):
+    # Logic to warn a user with the given reason
+    pass
+
+async def remove_warning(user, warning_id):
+    # Logic to remove a specific warning from a user
+    pass
+
+async def get_user_warnings(user):
+    # Logic to get all warnings of a user
+    pass
+
+async def clear_user_warnings(user):
+    # Logic to clear all warnings of a user
+    pass
+
+async def display_warnings(user):
+    # Logic to display all warnings of a user
+    pass
